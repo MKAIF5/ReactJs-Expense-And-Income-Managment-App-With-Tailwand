81c82af3a95a310ca7512be512babe25b0ad5077
mohammed kaif <mkaif555123@gmail.com>
2024-09-11 19:54:55 +0500

    15% Work Done

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ function App() {
   return (
     <>
       <div className="my-20">
+        <h1 className="text-black text-center text-5xl">Total Balance</h1>
+        <br />
         <h1 className="text-white text-center text-4xl">0.00</h1>
       </div>
-
-      <div>
-        <div>
-          <p>Expense</p>
+      <div className="flex justify-center gap-1">
+        <div className="bg-white w-36 text-center p-6">
+          <p className="font-medium text-xl">Expense</p>
+          <p className="text-green-400 font-semibold">00.000</p>
         </div>
-        <div>
-          <p>Income</p>
-        </div>
-        <div>
-          <p>Balance</p>
+        <div className="bg-white w-36 text-center p-6">
+          <p className="font-medium text-xl">Income</p>
+          <p className="text-red-700 font-semibold">00.000</p>
         </div>
 
       </div>
